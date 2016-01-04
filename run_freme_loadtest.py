a10b9e34ae23ead0b929f5cd8d10e29483b2fb9f
Jonathan <josaudergh@gmail.com>
2016-01-04 09:13:33 -0500

    Updated Loadtest and Dataset

--- run_freme_loadtest.py
+++ run_freme_loadtest.py
@@ -1,11 +1,14 @@
 from __future__ import division
 import math
-#Parameter
-min_threads=1
+
+#Parameters
+min_threads=5
 max_threads=110
 increment=5
 jmxfile="freme.jmx"
 csvfile="results.csv"
+composedlog="composed_summary_reports"
+dataset="sample_textfiles"
 #Imports, attempts to import matplotlib(plotting library)
 import csv, sys,os
 plot=False
@@ -16,23 +19,33 @@ try:
 except:
     print("Matplotlib not installed, will not plot runtimes")
 
-#Removes previous csv file
+#Removes previous csv and composed-log file
 os.system("rm "+csvfile)
+os.system("rm "+composedlog)
 
-#Runs jmeter with variable amount of threads
+#Runs jmeter with variable amount of threads, saves output as csv and append generate report to composedlog
+n_docs=len(os.listdir(dataset))
 k=0
-for n in range(min_threads,max_threads,increment):
-    os.system("jmeter -n -t "+jmxfile+ " -J n_threads="+str(i)+ " -J output_csv_name="+csvfile+" -J start_index="+str(k))
+for i in range(min_threads,max_threads,increment):
+    os.system("jmeter -n -t "+jmxfile+ " -J n_threads="+str(i)+ " -J output_csv_name="+csvfile+" -J start_index="+str(k)+" -p freme-loadtest.properties")
+    with open("jmeter.log","r") as f:
+        report=f.readlines()[-1][:-1]
+    os.system("echo \""+report+"\" >> "+composedlog)
     k+=i
-    k%=654
+    k%=n_docs
 
-import csv
 #Parses result
 with open(csvfile,'r') as csvfile:
     table=[x for x in csv.DictReader(csvfile)]
 
-#Calculates 
+#Helper function for getting throughput from Summary report in Composedlog
+get_throughput=lambda x:float(x[x.rfind("=")+1:x.find("/s")].strip().rstrip())
+
+#Calculates and reads summary report
+k=0
 results=[]
+with open(composedlog,"r") as comp_log:
+	throughputs=[get_throughput(line) for line in comp_log.readlines()]
 for n in range(min_threads,max_threads,increment):
     current=table[:n]
     #t = elapsed times for each request
@@ -57,25 +70,26 @@ for n in range(min_threads,max_threads,increment):
     time_per_request=total_time_taken/n
     
     #throughput = (n / total_time_taken)*1000
-    throughput= (n / total_time_taken)*1000
+    throughput= throughputs[k]
+    k+=1
     
     #kb / sec = avg_kb * throughput
     kb_sec=(kb_avg/1024)*throughput
     
     std_dev=math.sqrt(sum([(x*x)-t_avg for x in t])/n)
     
-    results.append([n,t_avg,kb_avg,time_per_request,throughput,kb_sec,std_dev])
+    results.append([n,t_avg,kb_avg,total_time_taken,throughput,time_per_request,std_dev])
 
 if plot:
-    titles=["Average Time per Request (ms)","Average Size of Request (bytes)", "Total time per Request (ms)","Throughput (requests/second)","kb/sec","Standard Deviation"]
+    titles=["Average Response time per Request (ms)","Average Size of Response (bytes)", "Total time taken for all requests (ms)","Throughput (requests/second)","Total time taken for all requests/ number of parallel requests","Standard Deviation"]
     fig=plt.figure()
     for e in range(6):
-        num=(200)+30+(e+1)
+        num=230+(e+1)
         print num
         print e
-        plt.subplot(num)
-        for r in results:
-            
-            plt.title(titles[e])
-            plt.plot(r[0],r[e+1],'ro')
+        ax=fig.add_subplot(num)
+        for r in results: 
+            ax.set_ylabel(titles[e])
+            ax.set_xlabel("Number of parallel requests")
+            ax.plot(r[0],r[e+1],'ro')
     plt.show()
